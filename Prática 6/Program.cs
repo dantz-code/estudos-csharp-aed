47e25e209081d742d22cd3d1c293f18f375d7468
Danton Rodrigues Diniz <danton.diniz@gmail.com>
2026-08-13 15:20:00 -0300

    Prática 6: identifica números pela soma das partes

---
+++ Prática 6/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int P1, P2;
+        int Soma;
+        int Quadrado;
+
+        for (int n = 1000; n <= 9999; n++)
+        {
+            P1 = n / 100;
+            P2 = n % 100;
+
+            Soma = P1 + P2;
+
+            Quadrado = Soma * Soma;
+
+            if (Quadrado == n)
+            {
+                Console.WriteLine($"O Número {n} possui essa característica...");
+            }
+        }
+    }
+}
